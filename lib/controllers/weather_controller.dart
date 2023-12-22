9838731309ea5325ba97a84b026ab6e5fd9a67e8
Mehmet Nuraydın <meanllbrl@gmail.com>
2023-12-23 02:45:16 +0300

    v2

--- lib/controllers/weather_controller.dart
+++ lib/controllers/weather_controller.dart
@@ -1,13 +1,7 @@
 // controllers/weather_controller.dart
 
-import '../models/weather_model.dart';
 import '../services/weather_service.dart';
 
 class WeatherController {
-  final WeatherService weatherService = WeatherService();
 
-  Future<Weather> getWeather() async {
-    final weatherJson = await weatherService.fetchWeather();
-    return Weather.fromJson(weatherJson);
-  }
 }
